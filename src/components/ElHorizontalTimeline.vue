0bdf129cd717a0edbdf1a900a0239ac7bc717602
David Moseler <davidmoseler@protonmail.com>
2020-08-27 22:33:03 -0300

    Use a render function to properly implement the reverse prop

--- src/components/ElHorizontalTimeline.vue
+++ src/components/ElHorizontalTimeline.vue
@@ -1,15 +1,27 @@
-<template>
-  <div class="timeline" :class="{'reverse': reverse}">
-    <slot />
-  </div>
-</template>
-
 <script>
 export default {
   name: 'el-horizontal-timeline',
 
   props: {
     "reverse": Boolean
+  },
+  render(createElement) {
+    let items = this.$slots.default
+    if(this.reverse){
+      items = items.reverse()
+    }
+    return createElement(
+      'div',
+      {
+        class: {
+          'timeline': true
+        }
+      },
+      items
+    )
+  },
+  mounted() {
+    console.log(this.$slots.default[0])
   }
 }
 </script>
@@ -18,9 +30,5 @@ export default {
 .timeline {
   counter-reset: stepCount;
   display: flex;
-}
-
-.reverse {
-  flex-direction: row-reverse;
 }
 </style>
